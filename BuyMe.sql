acbc3a7004e888caf69b5e85e4cb0e600770f32b
Mahek P <120302903+Mkpz@users.noreply.github.com>
2023-08-04 12:52:47 -0400

    Changes to Schema

--- BuyMe.sql
+++ BuyMe.sql
@@ -1,17 +1,15 @@
-DROP DATABASE IF EXISTS `BuyMe`;
-CREATE DATABASE IF NOT EXISTS `BuyMe` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `BuyMe`;
-
--- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
--- 
--- Host: localhost   Database: BuyMe
+CREATE DATABASE  IF NOT EXISTS `buyme` /*!40100 DEFAULT CHARACTER SET latin1 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `buyme`;
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: buyme
 -- ------------------------------------------------------
--- Server version	8.0.33-ARM64
+-- Server version	8.0.34
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -20,70 +18,19 @@ USE `BuyMe`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `users` (
-  `user_type` varchar(5) DEFAULT NULL,
-  `username` varchar(50) DEFAULT 'root',
-  `password` varchar(50) DEFAULT 'root',
-  PRIMARY KEY (`username`, `password`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES ('ADMIN', 'map616@cs', 'password123');
-INSERT INTO `users` VALUES ('END', 'test1@rut', 'password567');
-INSERT INTO `users` VALUES ('CR', 'test2@rut', 'password000');
-INSERT INTO `users` VALUES ('CR', 'test3@rut', 'password999');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `FAQs`
---
-
-DROP TABLE IF EXISTS `FAQs`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `FAQs` (
-  `answer` varchar(100) ,
-  `question` varchar(100) ,
-  PRIMARY KEY (`question`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `FAQs`
---
-
-LOCK TABLES `FAQs` WRITE;
-/*!40000 ALTER TABLE `FAQs` DISABLE KEYS */;
-INSERT INTO `FAQs` VALUES ('The answer is yes!', 'Do you sell shirts?'); 
-INSERT INTO `FAQs` VALUES ('The answer is no!', 'Do you sell hats?'); 
-/*!40000 ALTER TABLE `FAQs` ENABLE KEYS */;
-UNLOCK TABLES;
-
-
---
 -- Table structure for table `alerts`
 --
 
 DROP TABLE IF EXISTS `alerts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `alerts` (
-  `alert_id` varchar(3) NOT NULL DEFAULT '000',
-  `item_description` varchar(100),
-  PRIMARY KEY (`alert_id`)
+  `alert_id` int NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `message` varchar(100) NOT NULL DEFAULT '',
+  PRIMARY KEY (`alert_id`,`username`),
+  KEY `alert_username_fk_idx` (`username`),
+  CONSTRAINT `alert_username_fk` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -93,8 +40,43 @@ CREATE TABLE `alerts` (
 
 LOCK TABLES `alerts` WRITE;
 /*!40000 ALTER TABLE `alerts` DISABLE KEYS */;
-INSERT INTO `alerts` VALUES ('001', 'Blue Polo Shirt, Size: Medium'); 
 /*!40000 ALTER TABLE `alerts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `auction`
+--
+
+DROP TABLE IF EXISTS `auction`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `auction` (
+  `start_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `end_time` datetime NOT NULL,
+  `auction_id` int NOT NULL AUTO_INCREMENT,
+  `current_price` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `seller_username` varchar(50) DEFAULT NULL,
+  `buyer_username` varchar(50) DEFAULT NULL,
+  `manufacture_id` int NOT NULL,
+  `hidden_minimum_price` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `minimum_bid_increment` decimal(10,2) NOT NULL DEFAULT '0.01',
+  PRIMARY KEY (`auction_id`),
+  KEY `buyer_username_fk_idx` (`buyer_username`),
+  KEY `seller_username_fk_idx` (`seller_username`),
+  KEY `manufacture_id_fk_idx` (`manufacture_id`),
+  CONSTRAINT `buyer_username_fk` FOREIGN KEY (`buyer_username`) REFERENCES `users` (`username`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `manufacture_id_fk` FOREIGN KEY (`manufacture_id`) REFERENCES `clothes` (`manufacture_id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT `seller_username_fk` FOREIGN KEY (`seller_username`) REFERENCES `users` (`username`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `auction`
+--
+
+LOCK TABLES `auction` WRITE;
+/*!40000 ALTER TABLE `auction` DISABLE KEYS */;
+/*!40000 ALTER TABLE `auction` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -103,12 +85,17 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `bid`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `bid` (
-  `bid_id` varchar(5) NOT NULL DEFAULT '00000',
-  `username` varchar(50) DEFAULT 'root',
-  `amount` int,
-  PRIMARY KEY (`bid_id`, `username`)
+  `bid_id` int NOT NULL,
+  `auction_id` int NOT NULL,
+  `bidder_username` varchar(50) NOT NULL DEFAULT 'root',
+  `amount` decimal(10,2) DEFAULT '0.01',
+  PRIMARY KEY (`bid_id`,`auction_id`,`bidder_username`),
+  KEY `suction_id_fk_idx` (`auction_id`),
+  KEY `bidder_username_fk_idx` (`bidder_username`),
+  CONSTRAINT `auction_id_fk` FOREIGN KEY (`auction_id`) REFERENCES `auction` (`auction_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `bidder_username_fk` FOREIGN KEY (`bidder_username`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -118,36 +105,62 @@ CREATE TABLE `bid` (
 
 LOCK TABLES `bid` WRITE;
 /*!40000 ALTER TABLE `bid` DISABLE KEYS */;
-INSERT INTO `bid` VALUES ('00001', 'test1@rut', 500); 
 /*!40000 ALTER TABLE `bid` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `auction`
+-- Table structure for table `bidsetting`
 --
 
-DROP TABLE IF EXISTS `auction`;
+DROP TABLE IF EXISTS `bidsetting`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `auction` (
-  `start_time` datetime,
-  `end_time` datetime,
-  `auction_id` varchar(5),
-  `highest_bid` int,
-  `seller_username`  varchar(50) NOT NULL DEFAULT '0000',
-  `buyer_username`  varchar(4) NOT NULL DEFAULT '0000',
-  PRIMARY KEY (`auction_id`)
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bidsetting` (
+  `auction_id` int NOT NULL,
+  `bidder_username` varchar(50) NOT NULL,
+  `anonymousORnot` tinyint NOT NULL DEFAULT '0',
+  `autobiddingORnot` tinyint NOT NULL DEFAULT '0',
+  `sutobid_upper_limit` decimal(10,2) NOT NULL DEFAULT '0.01',
+  PRIMARY KEY (`auction_id`,`bidder_username`),
+  KEY `bidder_username_fk2_idx` (`bidder_username`),
+  CONSTRAINT `auction_id_fk2` FOREIGN KEY (`auction_id`) REFERENCES `auction` (`auction_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `bidder_username_fk2` FOREIGN KEY (`bidder_username`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `auction`
+-- Dumping data for table `bidsetting`
 --
 
-LOCK TABLES `auction` WRITE;
-/*!40000 ALTER TABLE `auction` DISABLE KEYS */;
-INSERT INTO `auction` VALUES ('1000-01-01 00:00:00.000000', '9999-12-31 23:59:59.499999', '00000', 100, 'test1@rut', 'test2@rut'); 
-/*!40000 ALTER TABLE `auction` ENABLE KEYS */;
+LOCK TABLES `bidsetting` WRITE;
+/*!40000 ALTER TABLE `bidsetting` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bidsetting` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `bottoms`
+--
+
+DROP TABLE IF EXISTS `bottoms`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bottoms` (
+  `type` enum('Activewear','Sweatpants','Jeans') DEFAULT NULL,
+  `waist_length` enum('XS','S','M','L','XL') DEFAULT NULL,
+  `rise_type` enum('High','Mid','Low') DEFAULT NULL,
+  `manufacture_id` int NOT NULL,
+  PRIMARY KEY (`manufacture_id`),
+  CONSTRAINT `manufacture_id_bottoms_fk` FOREIGN KEY (`manufacture_id`) REFERENCES `clothes` (`manufacture_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bottoms`
+--
+
+LOCK TABLES `bottoms` WRITE;
+/*!40000 ALTER TABLE `bottoms` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bottoms` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -156,10 +169,13 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `clothes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `clothes` (
-  `manufacture_id`  varchar(4) NOT NULL DEFAULT '0000',
-  `brand` varchar(50),
+  `manufacture_id` int NOT NULL AUTO_INCREMENT,
+  `brand` enum('Adidas','Calvin Klein','Nike','Levis') DEFAULT NULL,
+  `type` enum('Top','Bottom','Footwear') DEFAULT NULL,
+  `gender` enum('Women','Men') DEFAULT NULL,
+  `age` enum('Infants','Kids','Teenagers','Young Adults','30-50','60+') DEFAULT NULL,
   PRIMARY KEY (`manufacture_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -170,63 +186,35 @@ CREATE TABLE `clothes` (
 
 LOCK TABLES `clothes` WRITE;
 /*!40000 ALTER TABLE `clothes` DISABLE KEYS */;
-INSERT INTO `clothes` VALUES ('0001', 'Adidas'); 
 /*!40000 ALTER TABLE `clothes` ENABLE KEYS */;
 UNLOCK TABLES;
 
-
 --
--- Table structure for table `top`
+-- Table structure for table `faqs`
 --
 
-DROP TABLE IF EXISTS `top`;
+DROP TABLE IF EXISTS `faqs`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `top` (
-  `type_of_collar`  varchar(15),
-  `size` varchar(15),
-  `sleeve_length_inches` int,
-  `manufacture_id`  varchar(4) NOT NULL DEFAULT '0000',
-  PRIMARY KEY (`manufacture_id`),
-  FOREIGN KEY (`manufacture_id`) REFERENCES `clothes` (`manufacture_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `faqs` (
+  `answer` varchar(100) DEFAULT NULL,
+  `question` varchar(100) NOT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `qid` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`qid`),
+  KEY `username_fk_idx` (`username`),
+  CONSTRAINT `username_fk` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `top`
+-- Dumping data for table `faqs`
 --
 
-LOCK TABLES `top` WRITE;
-/*!40000 ALTER TABLE `top` DISABLE KEYS */;
-INSERT INTO `top` VALUES ('None', 'Medium', 41, '0001'); 
-/*!40000 ALTER TABLE `top` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `bottoms`
---
-
-DROP TABLE IF EXISTS `bottoms`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `bottoms` (
-  `type`  varchar(15),
-  `waist_length` varchar(15),
-  `rise_type` varchar(15),
-  `manufacture_id`  varchar(4) NOT NULL DEFAULT '0000',
-  PRIMARY KEY (`manufacture_id`),
-  FOREIGN KEY (`manufacture_id`) REFERENCES `clothes` (`manufacture_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `bottoms`
---
-
-LOCK TABLES `bottoms` WRITE;
-/*!40000 ALTER TABLE `bottoms` DISABLE KEYS */;
-INSERT INTO `bottoms` VALUES ('Jean', 32, 'Normal', '0001'); 
-/*!40000 ALTER TABLE `bottoms` ENABLE KEYS */;
+LOCK TABLES `faqs` WRITE;
+/*!40000 ALTER TABLE `faqs` DISABLE KEYS */;
+INSERT INTO `faqs` VALUES ('Yes','Do you sell this?','test1@rut',1),('No','Can I find this in red?','test5',2);
+/*!40000 ALTER TABLE `faqs` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -235,14 +223,14 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `footwear`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `footwear` (
-  `type_of_footwear`  varchar(15),
-  `size` int,
-  `lace_color` varchar(15),
-  `manufacture_id`  varchar(4) NOT NULL DEFAULT '0000',
+  `type_of_footwear` enum('Athletic shoes','Boots','Sneakers','Flats') DEFAULT NULL,
+  `size` enum('4','5','6','7','8','9','10','11') DEFAULT NULL,
+  `lace_color` enum('Black','Blue','Brown','Beige','Green','Red') DEFAULT NULL,
+  `manufacture_id` int NOT NULL,
   PRIMARY KEY (`manufacture_id`),
-  FOREIGN KEY (`manufacture_id`) REFERENCES `clothes` (`manufacture_id`)
+  CONSTRAINT `manufacture_id_footwear_fk` FOREIGN KEY (`manufacture_id`) REFERENCES `clothes` (`manufacture_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -252,10 +240,60 @@ CREATE TABLE `footwear` (
 
 LOCK TABLES `footwear` WRITE;
 /*!40000 ALTER TABLE `footwear` DISABLE KEYS */;
-INSERT INTO `footwear` VALUES ('Sneaker', 10, 'Blue', '0001'); 
 /*!40000 ALTER TABLE `footwear` ENABLE KEYS */;
 UNLOCK TABLES;
 
+--
+-- Table structure for table `top`
+--
+
+DROP TABLE IF EXISTS `top`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `top` (
+  `Neck_type` enum('High','Boat','Collared') DEFAULT NULL,
+  `size` enum('XS','S','M','L','XL') DEFAULT NULL,
+  `sleeve_length` enum('none','short','long','3/4') DEFAULT NULL,
+  `manufacture_id` int NOT NULL,
+  PRIMARY KEY (`manufacture_id`),
+  CONSTRAINT `manufacture_id_top_fk` FOREIGN KEY (`manufacture_id`) REFERENCES `clothes` (`manufacture_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `top`
+--
+
+LOCK TABLES `top` WRITE;
+/*!40000 ALTER TABLE `top` DISABLE KEYS */;
+/*!40000 ALTER TABLE `top` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `user_type` enum('END','CR','ADMIN') NOT NULL,
+  `username` varchar(50) NOT NULL DEFAULT 'root',
+  `password` varchar(50) NOT NULL DEFAULT 'root',
+  PRIMARY KEY (`username`,`password`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES ('ADMIN','map616@cs','password123'),('END','test1@rut','password567'),('CR','test2@rut','password000'),('CR','test3@rut','password999'),('END','test5','password333');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
@@ -265,3 +303,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
+-- Dump completed on 2023-08-04 12:51:50
